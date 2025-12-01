0173cd54154c460969e3d7bd5abe00b6356c9aeb
OisinStapleton-STUDE <G00365445@atu.ie>
2025-12-01 10:38:00 +0000

    Added LoyaltyPoints feature. Printout statements in main to display.

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -9,9 +9,22 @@ public class Main {
         Customer customer = new Customer("Oisín","Oranmore","0891234567", 66, true );
         System.out.println(customer.toString());
 
-        PreferredCustomer loyalCustomer = new PreferredCustomer("Alan", "Salthill", "0860987654", 55, true, 400, 0.10);
+        PreferredCustomer loyalCustomer = new PreferredCustomer("Alan", "Salthill", "0860987654", 55, true, 200, 0.10);
         System.out.println(loyalCustomer.toString());
 
+        System.out.println("\nBefore Customer Points: \n");
+        loyalCustomer.setName("Andrew");
+        loyalCustomer.setAddress("Loughrea");
+        loyalCustomer.setPhoneNumber("0834576893");
+        loyalCustomer.setCustomerNumber(88);
+        loyalCustomer.setMailingList(true);
+        System.out.println(loyalCustomer);
+
+        System.out.println("\nAfter Customer Points: \n");
+        loyalCustomer.addLoyaltyPoints(700);
+        loyalCustomer.getLoyaltyPoints();
+        loyalCustomer.getDiscountLevel();
+        System.out.println(loyalCustomer);
 
     }
 }
